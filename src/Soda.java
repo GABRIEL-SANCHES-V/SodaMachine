38c95da2dc72e8baa893d06648a06165b8657078
GABRIEL-SANCHES-V <gabriel.vinhal@outlook.com>
2022-10-06 12:00:47 -0300

    Class Soda

--- src/Soda.java
+++ src/Soda.java
@@ -1,2 +1,14 @@
 public class Soda {
+
+    private String sodaName;
+    private double sodaPrice;
+
+    protected Soda(String sodaName, double sodaPrice) {
+        this.sodaName = sodaName;
+        this.sodaPrice = sodaPrice;
+    }
+
+    protected void setChangeValueSoda(double newValueSoda){
+        this.sodaPrice = newValueSoda;
+    }
 }
